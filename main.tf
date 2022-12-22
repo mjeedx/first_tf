ca4525cfca3c23d9ea00c239aebee65726d1e7d2
Mykhailo Zarichnyi <mykhailo.zarichnyi@lseg.com>
2022-12-22 15:05:57 +0200

    1st

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+
+resource "aws_instance" "first_instance" {
+  ami           = "ami-070b208e993b59cea"
+  instance_type = "t2.micro"
+  associate_public_ip_address  = true
+  subnet_id = aws_subnet.public_subnet.id
+  #vpc_security_group_ids = 
+  key_name = "key"
+  tags = {
+    Name = "first_instance"
+  }
+}
+
+output "first_instance" {
+    value = aws_instance.first_instance.public_ip
+}
---
+++ network.tf
@@ -0,0 +1,102 @@
+resource "aws_vpc" "vpc_1" {
+  cidr_block = "10.10.0.0/16"
+
+  tags = {
+    Name = "vpc_1"
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+
+resource "aws_internet_gateway" "main_gw" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  tags = {
+    Name = "main_gw"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_rt.id 
+}
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.vpc_1.id
+
+    tags = {
+        Name = "Public_Rt"
+    }  
+}
+
+resource "aws_route" "default_route" {
+    route_table_id = aws_route_table.public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_gw.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc_1.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc_1.cidr_block]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_network_acl" "nacl_1" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.10.1.0/24"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.10.1.0/24"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  tags = {
+    Name = "nacl_1"
+  }
+}
